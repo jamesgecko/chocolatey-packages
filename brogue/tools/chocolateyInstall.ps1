9fc91d46748d3470e7e016af2e935995cfd3ba2f
James D <jamesgecko@gmail.com>
2014-01-13 00:26:44 -0500

    Update brogue to 1.7.3

--- brogue/tools/chocolateyInstall.ps1
+++ brogue/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 $packageName = 'brogue'
-$url = 'https://sites.google.com/site/broguegame/Brogue%20Windows%20v1.7.2.zip?attredirects=0'
+$url = 'https://sites.google.com/site/broguegame/Brogue-windows-v1.7.3.zip?attredirects=0&d=1'
 
 try {
 	$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
 	Install-ChocolateyZipPackage "$packageName" "$url" "$installDir"
-	Install-ChocolateyDesktopLink "$($installDir)\Brogue v1.7.2\brogue.exe"
+	Install-ChocolateyDesktopLink "$($installDir)\Brogue-windows-v1.7.3\brogue.exe"
 } catch {
 	throw
 }
